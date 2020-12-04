958855cecdfdec242283652a76d8a8926dec3641
Joanna Graczyk <joanna.graczyk@swmansion.com>
2020-12-04 14:16:26 +0100

    Basic folders

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import catsEyes from "../images/catsEyes";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <ul className="header_links">
+        <li>What is this?</li>
+        <li>News</li>
+        <li>Surprise</li>
+      </ul>
+    </div>
+  );
+};
